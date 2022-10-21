61f44b3450d00d42e9ce9b823b882bc121ccda9c
Anat Fradin <anatfradin@Anats-MBP.lan>
2022-10-21 20:37:04 +0300

    maman 11 question 1 logic code

---
+++ MAMAN_11_Question_1/src/maman_11_question_1/GameSceneController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package maman_11_question_1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author anatfradin
+ */
+public class GameSceneController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ MAMAN_11_Question_1/src/maman_11_question_1/Result.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package maman_11_question_1;
+
+/**
+ *
+ * @author anatfradin
+ */
+public class Result {
+    private boolean guessOK = false;
+    private int numberOfBool = 0;
+    private int numberOfPgia = 0;
+    private String errorString = null;
+
+    public String getErrorString() {
+        return errorString;
+    }
+    
+    public Result(boolean result, int bool, int pgia, String error){
+        guessOK = result;
+        numberOfBool = bool;
+        numberOfPgia = pgia;
+        errorString = error;
+    }
+    
+    public Result(boolean result, int bool, int pgia){
+        guessOK = result;
+        numberOfBool = bool;
+        numberOfPgia = pgia;
+    }
+
+    public boolean isGuessOK() {
+        return guessOK;
+    }
+
+    public int getNumberOfBool() {
+        return numberOfBool;
+    }
+
+    public int getNumberOfPgia() {
+        return numberOfPgia;
+    }
+    
+    @Override
+    public String toString(){
+        return this.errorString!= null?"ERROR ":"" + " Guess: " + 
+                this.guessOK + " Bul: " + this.numberOfBool + " Pgia: " + this.numberOfPgia;
+    } 
+    
+    
+}
