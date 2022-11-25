86145bdae8aa8a670ccc2fd38c2f41b581cc1d28
Anat Fradin <anat.fradin@gmail.com>
2022-11-25 14:03:53 +0200

    Submission MAMAN 12

---
+++ MAMAN_12_question1_1/src/main/java/AdditionExpression.java
@@ -0,0 +1,27 @@
+package com.mycompany.maman_12_question_1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author anatfradin
+ */
+public class AdditionExpression extends CompoundExpression{
+    
+    public AdditionExpression(Expression operand_1, Expression operand_2) {
+        super(operand_1, operand_2);
+    }
+    
+    @Override
+    public double calculate() {
+        return getOperand_1().calculate() + getOperand_2().calculate();
+    }
+    
+    @Override
+    public String toString() {
+        return getOperand_1().toString() + " + " + getOperand_2().toString();
+    }
+}
